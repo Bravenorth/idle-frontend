82d7b58a78ae0c0e5fc8d21ba93f564658ef01f1
Bravenorth <hubert.aurelien91@gmail.com>
2025-04-19 00:57:32 +0200

    front only

--- src/App.js
+++ src/App.js
@@ -1,38 +1,12 @@
-// src/App.js
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from 'react-router-dom';
-import { useContext } from 'react';
-import { UserContext } from './context/UserContext';
-
-import Home from './pages/home/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Game from './pages/game/Game';
 
 function App() {
-  const { user, hasEnteredGame, isLoading } = useContext(UserContext);
-
-  if (isLoading) {
-    return (
-      <div style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <p>Chargement...</p>
-      </div>
-    );
-  }
-
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/game"
-          element={
-            user && hasEnteredGame ? <Game /> : <Navigate to="/" replace />
-          }
-        />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<Game />} />
+        <Route path="*" element={<Game />} />
       </Routes>
     </Router>
   );
